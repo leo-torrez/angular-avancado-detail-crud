c322ff8a90d6bbe6a854c779264d1e55d0e77889
Léo Torrez <leo@leotorrez.com.br>
2018-11-24 16:28:33 -0300

    adicionando in-memory-api angular

---
+++ src/app/pages/in-memory-database.ts
@@ -0,0 +1,35 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories = [
+      {
+        id: 1,
+        name: 'Moradia',
+        description: 'Pagamentos de contas da Casa'
+      },
+      {
+        id: 2,
+        name: 'Sáude',
+        description: 'Plano de Saúde e Remédios'
+      },
+      {
+        id: 3,
+        name: 'Lazer',
+        description: 'Cinema, parque, praia'
+      },
+      {
+        id: 4,
+        name: 'Salário',
+        description: 'Recebimento de Salário'
+      },
+      {
+        id: 5,
+        name: 'Freelas',
+        description: 'Trabalhos como freelancer'
+      }
+    ];
+
+    return { categories };
+  }
+}
