5ace7e3cf942818fa0f7315026ec4682d4d0e205
Léo Torrez <leo@leotorrez.com.br>
2018-11-24 16:49:40 -0300

    adicionando serviço do categorylist e o interceptor do in-memory-angular-web-api ; "

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesModule } from './pages/categories/categories.module';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CategoriesModule
+    CategoriesModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
